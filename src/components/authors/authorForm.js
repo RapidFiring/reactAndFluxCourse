b69b1115da636395487a690f35e79766e39cfd37
Rapid Firing <rapid.firing.eve@gmail.com>
2016-07-22 16:17:04 +0200

    Finishing React Forms Typos & Spelling

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -11,7 +11,7 @@ var AuthorForm = React.createClass({
         author: React.PropTypes.object.isRequired,
         onChange: React.PropTypes.func.isRequired,
         onSave: React.PropTypes.func.isRequired,
-        errors: React.PropTypes.object
+        error: React.PropTypes.object
     },
 
   render: function() {
@@ -23,13 +23,13 @@ var AuthorForm = React.createClass({
                        label="First Name"
                        value={this.props.author.firstName}
                        onChange={this.props.onChange}
-                       error={this.props.errors.firstName}/>
+                       error={this.props.error.firstName}/>
 
                 <Input name="lastName"
                        label="Last Name"
                        value={this.props.author.lastName}
                        onChange={this.props.onChange}
-                       error={this.props.errors.lastName}/>
+                       error={this.props.error.lastName}/>
 
               <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave}/>
           </form>
